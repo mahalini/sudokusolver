22003f537ad6ee8c983012bd890682ccade2c0f4
bobby. <75751816+mahalini@users.noreply.github.com>
2026-01-10 02:40:21 +0700

    Change server port from 3000 to 5000

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(function(req, res, next) {
 });
 
 //Start our server and tests!
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0'; // Bind to all interfaces for Replit
 app.listen(PORT, HOST, function () {
   console.log("Listening on port " + PORT);
